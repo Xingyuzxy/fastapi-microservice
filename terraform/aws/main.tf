7643a47d163449493650afe0977442f77f8384ab
Your Name <you@example.com>
2026-07-26 11:55:28 -0400

    corrigate terraform files

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -1,16 +1,15 @@
 resource "aws_vpc" "vpc_network_eks" {
-    provider = aws.us-east-1
-    cidr_block = "10.0.1.0/16"
+    cidr_block = "10.0.0.0/16"
 }
 
 resource "aws_subnet" "my_subnet" {
     vpc_id                  = aws_vpc.vpc_network_eks.id
-    cidr_block              = "10.0.1.0/28" 
+    cidr_block              = "10.0.0.0/28" 
 }
 
 resource "aws_instance" "instance1" {
 
     ami           = "ami-0fe3999acbfd542d0"
-    instance_type = "t2.micro"
+    instance_type = "t3.micro"
     subnet_id = aws_subnet.my_subnet.id
 }

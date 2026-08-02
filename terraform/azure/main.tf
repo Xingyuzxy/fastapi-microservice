fec3b65c974f75a2c5de68b69c8946bfd498fc0b
Your Name <you@example.com>
2026-08-02 14:53:00 -0400

    add first azure setting

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -1,5 +1,49 @@
 # Create Resource Group 
-resource "azurerm_resource_group" "my_demo_rg1" {
-    location = "eastus"
-    name = "my-demo-rg1"  
+
+resource "azurerm_virtual_network" "myvnet" {
+  name                = "myvnet-1"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
+}
+
+# Resource-3: Create Subnet
+resource "azurerm_subnet" "mysubnet" {
+  name                 = "mysubnet-1"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.myvnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+# Resource-4: Create Public IP Address
+resource "azurerm_public_ip" "mypublicip" {
+  name                = "mypublicip-1"
+  resource_group_name = var.resource_group_name
+  location            = var.resource_group_location
+  allocation_method   = "Static"
+  tags = {
+    environment = "Dev"
+  }
+}
+
+# Resource-5: Create Network Interface
+resource "azurerm_network_interface" "myvm1nic" {
+  name                = "vm1-nic"
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.mysubnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.mypublicip.id 
+  }
+}
+
+resource "azurerm_storage_account" "mysa" {
+  name                     = "mysa23"
+  resource_group_name      = var.resource_group_name
+  location                 = var.resource_group_location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
---
+++ terraform/azure/variable.tf
@@ -0,0 +1,12 @@
+variable "resource_group_location" {
+  type        = string
+  default     = "eastus"
+  description = "Location of the resource group."
+}
+
+
+variable "resource_group_name" {
+  type        = string
+  default     = "kml_rg_main-ef53d344888d40fa"
+  description = "Resource group name in your Azure subscription."
+}
